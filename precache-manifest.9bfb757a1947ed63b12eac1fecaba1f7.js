f5f72d221222e7e4831e22defa070ce39d68f0bc
anuragkothare <anuragkothare@outlook.com>
2020-05-24 03:35:10 +0530

    Updates

---
+++ precache-manifest.9bfb757a1947ed63b12eac1fecaba1f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb952ebe22a68ee69c9b17189f1890d",
+    "url": "/Covid-19-App/index.html"
+  },
+  {
+    "revision": "15edf05792dd528cb4b8",
+    "url": "/Covid-19-App/static/css/main.ab647db0.chunk.css"
+  },
+  {
+    "revision": "93b9fcb1704e368bc055",
+    "url": "/Covid-19-App/static/js/2.e972eebf.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-App/static/js/2.e972eebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15edf05792dd528cb4b8",
+    "url": "/Covid-19-App/static/js/main.b9b40a67.chunk.js"
+  },
+  {
+    "revision": "0011ebc82e0f6ede674f",
+    "url": "/Covid-19-App/static/js/runtime-main.733b14a1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-App/static/media/corona.d7265326.png"
+  }
+]);
